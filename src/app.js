29d8ce4874a53a70349f289a31aee957cb4511b9
Franz Seidel <franz8818@gmail.com>
2022-08-09 00:45:29 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let object = ["my homework", "my work", "my phone", "my girlfriend"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who1 = Math.floor(Math.random() + 3);
+  let what1 = Math.floor(Math.random() + 3);
+  let object1 = Math.floor(Math.random() + 3);
+  let when1 = Math.floor(Math.random() + 3);
+
+  document.getElementById("excuse").innerHTML =
+    who[who1] + what[what1] + object[object1] + when[when1];
 };
